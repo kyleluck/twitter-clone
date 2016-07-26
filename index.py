cb8e2c15d79f324b7391a7b59d4a2ff33b8be953
Kyle Luck <luck.kyle@gmail.com>
2016-07-26 09:03:55 -0400

    adding initial login/signup functionality

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, session, render_template, request
+from werkzeug.security import generate_password_hash, check_password_hash
 import pg
 
 db = pg.DB(dbname='twitter')
@@ -15,17 +16,46 @@ def show_public():
 def login():
     return render_template('login.html')
 
-@app.route('/processlogin')
+@app.route('/processlogin', methods=['POST'])
 def process_login():
-    return 'process_login'
+    username = request.form['username']
+    password = request.form['password']
+
+    # find user by username
+    user = db.query("select username, password from user_table where username = '%s'" % username)
+    if len(user.namedresult()) >= 1:
+        # check that password matches. if so, redirect to home page otherwise redirect to login
+        check_password = check_password_hash(user.namedresult()[0].password, password)
+        if check_password:
+            session['user'] = username
+            return redirect('/')
+        else:
+            return redirect('/login')
 
 @app.route('/signup')
 def signup():
-    return 'signup page'
+    return render_template('signup.html')
+
+@app.route('/processsignup', methods=['POST'])
+def process_signup():
+    username = request.form['username']
+    password = request.form['password']
+    userfull = request.form['userfull']
+    bio = request.form['bio']
+    website = request.form['website']
+    hashed_password = generate_password_hash(password)
+
+    # insert user into the database
+    db.insert('user_table', username=username, password=hashed_password, userfull=userfull, bio=bio, website=website)
+
+    return redirect('/login')
 
 @app.route('/timeline')
 def timeline():
     return 'timeline page'
+
+# Secret key for session
+app.secret_key = 'CSF686CCF85C6FRTCHQDBJDXHBHC1G478C86GCFTDCR'
 
 if __name__ == '__main__':
     app.run(debug=True)
