908ff9057f39c529b5fbc253be5636afdbebad3d
Kyle Luck <luck.kyle@gmail.com>
2016-07-27 13:16:40 -0400

    seperating timeline and public templates

--- index.py
+++ index.py
@@ -29,7 +29,7 @@ def show_public():
                 user_table on tweet.user_id = user_table.id
             order by
                 tweet.created_at desc''').namedresult()
-        return render_template('timeline.html',
+        return render_template('public.html',
             title = "What's Happening",
             tweets = tweets)
     else:
